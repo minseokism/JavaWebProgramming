6154e1a6af88777566960b41ebc9d55f0d0dae0e
Minseok Lee <lagender@gmail.com>
2016-02-02 17:24:57 +0900

    3.6.3 계산기 서블릿 작성

--- web03/src/lesson03/servlets/HelloWorld.java
+++ web03/src/lesson03/servlets/HelloWorld.java
@@ -2,25 +2,14 @@ package lesson03.servlets;
 
 import java.io.IOException;
 
-import javax.servlet.Servlet;
+import javax.servlet.GenericServlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
-public class HelloWorld implements Servlet{
+public class HelloWorld extends GenericServlet{
 	ServletConfig config;
-	
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		System.out.println("init() 호출됨");
-		this.config=config;
-	}
-	
-	@Override
-	public void destroy() {
-		System.out.println("destroy() 호출됨");
-	}
 	
 	@Override
 	public void service(ServletRequest request, ServletResponse response)
@@ -28,16 +17,5 @@ public class HelloWorld implements Servlet{
 		System.out.println("service() 호출됨");
 	}
 	
-	@Override
-	public ServletConfig getServletConfig() {
-		System.out.println("getServletConfig() 호출됨");
-		return this.config;
-	}
-	
-	@Override
-	public String getServletInfo() {
-		System.out.println("getServletInfo() 호출됨");
-		return "version=1.0;author=minseokLee;copyright=minseokLee 2016";
-	}
 
 }
