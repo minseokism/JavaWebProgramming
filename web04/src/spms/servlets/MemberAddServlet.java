75aa325b9e604fb23741e987590607c7e02ca68e
Minseok Lee <lagender@gmail.com>
2016-02-16 16:44:16 +0900

    4.4.3 한글 깨짐 해결책

--- web04/src/spms/servlets/MemberAddServlet.java
+++ web04/src/spms/servlets/MemberAddServlet.java
@@ -37,6 +37,8 @@ public class MemberAddServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		
