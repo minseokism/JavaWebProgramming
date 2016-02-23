80c826bcd154d43e95cf149e4faff6267bc1b3f8
Minseok Lee <lagender@gmail.com>
2016-02-23 15:39:52 +0900

    4.9.3 필터의 구동 - 애노테이션을 이용한 방법

--- web04/src/spms/filters/CharacterEncodingFilter.java
+++ web04/src/spms/filters/CharacterEncodingFilter.java
@@ -8,7 +8,14 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 
+@WebFilter(
+		urlPatterns="/*",
+		initParams={
+				@WebInitParam(name="encoding", value="UTF-8")
+		})
 public class CharacterEncodingFilter implements Filter {
 	FilterConfig config;
 	
