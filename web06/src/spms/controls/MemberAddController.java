e7e09064d15a921c30a2afabcf5721ed5bb05a99
Minseok Lee <lagender@gmail.com>
2016-03-18 16:54:18 +0900

    6.4.3. 페이지 컨트롤러의 DataBinding 구현

--- web06/src/spms/controls/MemberAddController.java
+++ web06/src/spms/controls/MemberAddController.java
@@ -2,22 +2,32 @@ package spms.controls;
 
 import java.util.Map;
 
+import spms.bind.DataBinding;
 import spms.dao.MySqlMemberDao;
 import spms.vo.Member;
 
-public class MemberAddController implements Controller{
+public class MemberAddController implements Controller, DataBinding{
 	MySqlMemberDao memberDao;
+	
 	
 	public MemberAddController setMemberDao(MySqlMemberDao memberDao){
 		this.memberDao = memberDao;
 		return this;
 	}
+	
+	@Override
+	public Object[] getDataBinders() {
+		return new Object[]{
+				"member", spms.vo.Member.class
+		};
+	}
+	
 	@Override
 	public String execute(Map<String, Object> model) throws Exception {
-		if (model.get("member") == null) {
+		Member member = (Member)model.get("member");
+		if (member.getEmail() == null) {
 			return "/member/MemberForm.jsp";
 		} else {
-			Member member = (Member)model.get("member");
 			memberDao.insert(member);
 			
 			return "redirect:list.do";
